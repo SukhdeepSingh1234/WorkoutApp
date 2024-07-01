aa4f6132a06223fd4d0b7bd60a6cb1b2fe7d8b23
SukhdeepSingh1234 <ss6540029@gmail.com>
2024-07-01 21:05:35 +0530

    Added Gifs

---
+++ app/assets/links.js
@@ -0,0 +1,10 @@
+
+
+
+
+export default links =[
+    {
+        dumbell_fly:"https://i.pinimg.com/originals/0b/cb/fc/0bcbfcfb99c70ae8ec9c7223b965d5ff.gif"
+
+    }
+]
--- app/screens/Challenges.js
+++ app/screens/Challenges.js
@@ -1,12 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Screen from '../components/Screen'
+
 
 export default function Challenges() {
   return (
-    <View>
-      <Text>Challenges</Text>
+    <Screen>
+      <View style={styles.container}>
+      <Image
+        source={require("../assets/gifs/chest/dumbell_fly.gif")} 
+        style={styles.gif}
+      />
     </View>
+    </Screen>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    backgroundColor:"orange",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  gif: {
+    width: 200,
+    height: 200,
+    backgroundColor: "purple",
+  },
+})
